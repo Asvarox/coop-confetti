c11723ca71a689cc81c756257cb8bbd5b2ddbeb5
Aleksander Tatarczyk <tatarczyk.aleksander@gmail.com>
2025-04-09 14:05:35 +0200

    Updates

--- assets/iframe-renderer-ae1be3c9.js
+++ assets/iframe-renderer-ae1be3c9.js
@@ -90,7 +90,7 @@
                 </div>
                 <div style="display: flex; gap: 1rem;">
                     ${["0.5","1","2","3","5","8","13","21","❓","☕","Skip"].map(s=>W`
-                            <button style="padding: 1rem;font-size: 2rem;${this._estimate===s?"background: lightgreen;":""}" @click="${()=>this.setEstimate(s)}">${s}</button>
+                            <button style="padding: 1rem;font-size: 2rem;${this._estimate===s?"background: lightgreen;":""}" @click="${()=>this.setEstimate(s)}" disabled="${this._revealed}">${s}</button>
                         `)}
                     <button style="padding: 1rem;font-size: 2rem;" @click="${()=>this.setEstimate(null)}">Reset</button>
                 </div>
